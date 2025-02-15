86cef19dc2bcc241463c192001e6633db9bd51a7
Gopi Trinadh <gopitrinadhn@gmail.com>
2025-02-15 22:49:54 +0530

    RDD Transformations done

---
+++ RDD_Actions.py
@@ -0,0 +1,32 @@
+# ACTIONS
+# (collect,count,take,first,foreach,reduce,saveAsTextFile,saveAsSequenceFile)
+
+# from pyspark.sql import SparkSession
+
+# spark =SparkSession.builder.appName("RDD_Actions").getOrCreate()
+
+# rdd =spark.sparkContext.parallelize([1,2,3,4,5,6])
+
+# print(rdd.collect())
+
+# print(rdd.count())
+
+# print(rdd.take(3))
+
+# print(rdd.first())
+
+# print(rdd.foreach(lambda x:print(x)))
+
+# print(rdd.reduce(lambda x,y:x+y))
+
+# print(rdd.saveAsTextFile("file_path"))
+
+# print(rdd.saveAsSequenceFile('file_path'))
+
+
+
+
+
+
+
+
